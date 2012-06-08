822091a2f2b1a0a2b9ef20aa554017d224e614d0
Bharath M R <catchmrbharath@gmail.com>
2012-06-08 23:17:09 +0530

    changed functions to track continuity and domain

--- sympy/plotting/intervalmath/lib_interval.py
+++ sympy/plotting/intervalmath/lib_interval.py
@@ -10,81 +10,108 @@ def exp(x):
 
 #Monotonic
 def log(x):
-    x = interval(x)
-    if x.end <= 0:
-        return inteval(-np.inf, -np.inf)
-    elif x.start <= 0:
-        return interval(-np.inf, np.log(x.end))
-    else:
+    if isinstance(x, (int, float)):
+        if x <= 0:
+            return interval(-np.inf, np.inf, is_valid = False)
+        else:
+            return interval(np.log(x))
+    elif isinstance(x, interval):
+        if not x.is_valid:
+            return interval(-np.inf, np.inf, is_valid = x.is_valid)
+        elif x.end <= 0:
+            return interval(-np.inf, np.inf, is_valid = False)
+        elif x.start <= 0:
+            return interval(-np.inf, np.inf, is_valid = None)
+
         return interval(np.log(x.start), np.log(x.end))
 
 #Monotonic
 def log10(x):
-    x = interval(x)
-    if x.end <= 0:
-        return inteval(-np.inf, -np.inf)
-    elif x.start <= 0:
-        return interval(-np.inf, np.log10(x.end))
-    else:
+    if isinstance(x, (int, float)):
+        if x <= 0:
+            return interval(-np.inf, np.inf, is_valid = False)
+        else:
+            return interval(np.log(x))
+    elif isinstance(x, interval):
+        if not x.is_valid:
+            return interval(-np.inf, np.inf, is_valid = x.is_valid)
+        elif x.end <= 0:
+            return interval(-np.inf, np.inf, is_valid = False)
+        elif x.start <= 0:
+            return interval(-np.inf, np.inf, is_valid = None)
         return interval(np.log10(x.start), np.log10(x.end))
+    else:
+        raise NotImplemented
 
 #Monotonic
 def atan(x):
-    x = interval(x)
-    return interval(np.atan(x.start), np.atan(x.end))
+    if isinstance(x, (int, float)):
+        return interval(np.arctan(x))
+    elif isinstance(x, interval):
+        start = np.arctan(x,start)
+        end = np.arctan(x.end)
+        return interval(start, end, is_valid = x.is_valid)
+    else:
+        raise NotImplemented
 
 
 #periodic
 def sin(x):
-    x = interval(x)
-    if not (np.isfinite(x.start) and np.isfinite(x.end)):
-        return interval(-1, 1)
-    na, a = divmod(x.start, np.pi / 2.0)
-    nb, b = divmod(x.end, np.pi / 2.0)
-    start = min(np.sin(x.start), np.sin(x.end))
-    end = max(np.sin(x.start), np.sin(x.end))
-#    print na, nb
-    if nb - na > 4:
-        return interval(-1, 1)
-    elif na == nb:
-        return interval(start, end)
+    if isinstance(x, (int, float)):
+        return interval(np.sin(x))
+    elif isinstance(x, interval):
+        if not (np.isfinite(x.start) and np.isfinite(x.end)):
+            return interval(-1, 1, is_valid = x.is_valid)
+        na, a = divmod(x.start, np.pi / 2.0)
+        nb, b = divmod(x.end, np.pi / 2.0)
+        start = min(np.sin(x.start), np.sin(x.end))
+        end = max(np.sin(x.start), np.sin(x.end))
+        if nb - na > 4:
+            return interval(-1, 1, is_valid = x.is_valid)
+        elif na == nb:
+            return interval(start, end, x.is_valid)
+        else:
+            if (na - 1) // 4 != (nb - 1) // 4:
+                #sin has max
+                end = 1
+            if (na - 3) // 4 != (nb - 3) // 4:
+                #sin has min
+                start = -1
+            return interval(start, end, x.is_valid)
     else:
-        #print na, nb
-        if (na - 1) // 4 != (nb - 1) // 4:
-            #sin has max
-            end = 1
-        if (na - 3) // 4 != (nb - 3) // 4:
-            #sin has min
-            start = -1
-        return interval(start, end)
+        raise NotImplemented
 
 #periodic
 def cos(x):
-    x = interval(x)
-    if not (np.isfinite(x.start) and np.isfinite(x.end)):
-        return interval(-1, 1)
-    na, a = divmod(x.start, np.pi / 2.0)
-    nb, b = divmod(x.end, np.pi / 2.0)
-    if x.start < 0:
-        na = -1 - na
-    if x.end < 0:
-        nb = -1 - nb
-    start = min(np.cos(x.start), np.cos(x.end))
-    end = max(np.cos(x.start), np.cos(x.end))
-    if nb - na > 4:
-        #differ more than 2*pi
-        return interval(-1, 1)
-    elif na == nb:
-        #in the same quadarant
-        return interval(start, end)
+    if isinstance(x, (int, float)):
+        return interval(np.sin(x))
+    elif isinstance(x, interval):
+        if not (np.isfinite(x.start) and np.isfinite(x.end)):
+            return interval(-1, 1, is_valid = x.is_valid)
+        na, a = divmod(x.start, np.pi / 2.0)
+        nb, b = divmod(x.end, np.pi / 2.0)
+        if x.start < 0:
+            na = -1 - na
+        if x.end < 0:
+            nb = -1 - nb
+        start = min(np.cos(x.start), np.cos(x.end))
+        end = max(np.cos(x.start), np.cos(x.end))
+        if nb - na > 4:
+            #differ more than 2*pi
+            return interval(-1, 1, is_valid = x.is_valid)
+        elif na == nb:
+            #in the same quadarant
+            return interval(start, end, is_valid = x.is_valid)
+        else:
+            if (na) // 4 != (nb) // 4:
+                #cos has max
+                end = 1
+            if (na - 2) // 4 != (nb - 2) // 4:
+                #cos has min
+                start = -1
+            return interval(start, end, is_valid = x.is_valid)
     else:
-        if (na) // 4 != (nb) // 4:
-            #cos has max
-            end = 1
-        if (na - 2) // 4 != (nb - 2) // 4:
-            #cos has min
-            start = -1
-        return interval(start, end)
+        raise NotImplemented
 
 def tan(x):
     return sin(x) / cos(x)
