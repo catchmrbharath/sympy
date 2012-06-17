74ce05e85b33c3b09999f7305ba83f79fb1da0c4
Bharath M R <catchmrbharath@gmail.com>
2012-06-17 12:20:12 +0530

    added inverse hyperbolic functions. pep8 fixes.

--- sympy/plotting/intervalmath/lib_interval.py
+++ sympy/plotting/intervalmath/lib_interval.py
@@ -1,56 +1,60 @@
 from interval_arithmetic import interval
-import numpy as np
+from sympy.external import import_module
 #Library for interval arithmetic. Contains all the implemented functions for
 #interval arithmetic
 
+np = import_module('numpy')
 #Monotonic
 def exp(x):
     """evaluates the exponential of an interval"""
     if isinstance(x, (int, float)):
         return interval(np.exp(x), np.exp(x))
     elif isinstance(x, interval):
-        return interval(np.exp(x.start), np.exp(x.end), is_valid = x.is_valid)
+        return interval(np.exp(x.start), np.exp(x.end), is_valid=x.is_valid)
     else:
         raise NotImplementedError
+
 
 #Monotonic
 def log(x):
     """evauates the natural logarithm of an interval"""
     if isinstance(x, (int, float)):
         if x <= 0:
-            return interval(-np.inf, np.inf, is_valid = False)
+            return interval(-np.inf, np.inf, is_valid=False)
         else:
             return interval(np.log(x))
     elif isinstance(x, interval):
         if not x.is_valid:
-            return interval(-np.inf, np.inf, is_valid = x.is_valid)
+            return interval(-np.inf, np.inf, is_valid=x.is_valid)
         elif x.end <= 0:
-            return interval(-np.inf, np.inf, is_valid = False)
+            return interval(-np.inf, np.inf, is_valid=False)
         elif x.start <= 0:
-            return interval(-np.inf, np.inf, is_valid = None)
+            return interval(-np.inf, np.inf, is_valid=None)
 
         return interval(np.log(x.start), np.log(x.end))
     else:
         raise NotImplementedError
+
 
 #Monotonic
 def log10(x):
     """evaluates the logarithm to the base 10 of an interval"""
     if isinstance(x, (int, float)):
         if x <= 0:
-            return interval(-np.inf, np.inf, is_valid = False)
+            return interval(-np.inf, np.inf, is_valid=False)
         else:
             return interval(np.log(x))
     elif isinstance(x, interval):
         if not x.is_valid:
-            return interval(-np.inf, np.inf, is_valid = x.is_valid)
+            return interval(-np.inf, np.inf, is_valid=x.is_valid)
         elif x.end <= 0:
-            return interval(-np.inf, np.inf, is_valid = False)
+            return interval(-np.inf, np.inf, is_valid=False)
         elif x.start <= 0:
-            return interval(-np.inf, np.inf, is_valid = None)
+            return interval(-np.inf, np.inf, is_valid=None)
         return interval(np.log10(x.start), np.log10(x.end))
     else:
         raise NotImplementedError
+
 
 #Monotonic
 def atan(x):
@@ -60,7 +64,7 @@ def atan(x):
     elif isinstance(x, interval):
         start = np.arctan(x.start)
         end = np.arctan(x.end)
-        return interval(start, end, is_valid = x.is_valid)
+        return interval(start, end, is_valid=x.is_valid)
     else:
         raise NotImplementedError
 
@@ -72,15 +76,15 @@ def sin(x):
         return interval(np.sin(x))
     elif isinstance(x, interval):
         if not (np.isfinite(x.start) and np.isfinite(x.end)):
-            return interval(-1, 1, is_valid = x.is_valid)
+            return interval(-1, 1, is_valid=x.is_valid)
         na, __ = divmod(x.start, np.pi / 2.0)
         nb, __ = divmod(x.end, np.pi / 2.0)
         start = min(np.sin(x.start), np.sin(x.end))
         end = max(np.sin(x.start), np.sin(x.end))
         if nb - na > 4:
-            return interval(-1, 1, is_valid = x.is_valid)
+            return interval(-1, 1, is_valid=x.is_valid)
         elif na == nb:
-            return interval(start, end, is_valid = x.is_valid)
+            return interval(start, end, is_valid=x.is_valid)
         else:
             if (na - 1) // 4 != (nb - 1) // 4:
                 #sin has max
@@ -88,9 +92,10 @@ def sin(x):
             if (na - 3) // 4 != (nb - 3) // 4:
                 #sin has min
                 start = -1
-            return interval(start, end, is_valid = x.is_valid)
+            return interval(start, end, is_valid=x.is_valid)
     else:
         raise NotImplementedError
+
 
 #periodic
 def cos(x):
@@ -99,21 +104,17 @@ def cos(x):
         return interval(np.sin(x))
     elif isinstance(x, interval):
         if not (np.isfinite(x.start) and np.isfinite(x.end)):
-            return interval(-1, 1, is_valid = x.is_valid)
+            return interval(-1, 1, is_valid=x.is_valid)
         na, __ = divmod(x.start, np.pi / 2.0)
         nb, __ = divmod(x.end, np.pi / 2.0)
-        if x.start < 0:
-            na = -1 - na
-        if x.end < 0:
-            nb = -1 - nb
         start = min(np.cos(x.start), np.cos(x.end))
         end = max(np.cos(x.start), np.cos(x.end))
         if nb - na > 4:
             #differ more than 2*pi
-            return interval(-1, 1, is_valid = x.is_valid)
+            return interval(-1, 1, is_valid=x.is_valid)
         elif na == nb:
             #in the same quadarant
-            return interval(start, end, is_valid = x.is_valid)
+            return interval(start, end, is_valid=x.is_valid)
         else:
             if (na) // 4 != (nb) // 4:
                 #cos has max
@@ -121,13 +122,15 @@ def cos(x):
             if (na - 2) // 4 != (nb - 2) // 4:
                 #cos has min
                 start = -1
-            return interval(start, end, is_valid = x.is_valid)
+            return interval(start, end, is_valid=x.is_valid)
     else:
         raise NotImplementedError
+
 
 def tan(x):
     """Evaluates the tan of an interval"""
     return sin(x) / cos(x)
+
 
 #Monotonic
 def sqrt(x):
@@ -136,14 +139,15 @@ def sqrt(x):
         return interval(np.sqrt(x))
     elif isinstance(x, interval):
         if x.end < 0:
-            return interval(-np.inf, np.inf, is_valid = False)
+            return interval(-np.inf, np.inf, is_valid=False)
         elif x.start < 0:
-            return interval(-np.inf, np.inf, is_valid = None)
+            return interval(-np.inf, np.inf, is_valid=None)
         else:
             return interval(np.sqrt(x.start), np.sqrt(x.end),
-                    is_valid = x.is_valid)
+                    is_valid=x.is_valid)
     else:
         raise NotImplementedError
+
 
 def imin(*args):
     """Evaluates the minimum of a list of intervals"""
@@ -154,15 +158,16 @@ def imin(*args):
                     or a.is_valid]
         if len(new_args) == 0:
             if all(a.is_valid is False for a in args):
-                return interval(-np.inf, np.inf, is_valid = False)
+                return interval(-np.inf, np.inf, is_valid=False)
             else:
-                return interval(-np.inf, np.inf, is_valid = None)
+                return interval(-np.inf, np.inf, is_valid=None)
         start_array = [a if isinstance(a, (int, float)) else a.start
                         for a in new_args]
 
         end_array = [a if isinstance(a, (int, float)) else a.end
                         for a in new_args]
         return interval(min(start_array), min(end_array))
+
 
 def imax(*args):
     """Evaluates the maximum of a list of intervals"""
@@ -173,9 +178,9 @@ def imax(*args):
                     or a.is_valid]
         if len(new_args) == 0:
             if all(a.is_valid is False for a in args):
-                return interval(-np.inf, np.inf, is_valid = False)
+                return interval(-np.inf, np.inf, is_valid=False)
             else:
-                return interval(-np.inf, np.inf, is_valid = None)
+                return interval(-np.inf, np.inf, is_valid=None)
         start_array = [a if isinstance(a, (int, float)) else a.start
                         for a in new_args]
 
@@ -184,15 +189,17 @@ def imax(*args):
 
         return interval(max(start_array), max(end_array))
 
+
 #Monotonic
 def sinh(x):
     """Evaluates the hyperbolic sine of an interval"""
     if isinstance(x, (int, float)):
         return interval(np.sinh(x), np.sinh(x))
     elif isinstance(x, interval):
-        return interval(np.sinh(x.start), np.sinh(x.end), is_valid = x.is_valid)
+        return interval(np.sinh(x.start), np.sinh(x.end), is_valid=x.is_valid)
     else:
         raise NotImplementedError
+
 
 def cosh(x):
     """Evaluates the hyperbolic cos of an interval"""
@@ -202,12 +209,15 @@ def cosh(x):
         #both signs
         if x.start < 0 and x.end > 0:
             end = max(np.cosh(x.start), np.cosh(x.end))
-            return interval(1, end, is_valid = x.is_valid)
+            return interval(1, end, is_valid=x.is_valid)
         else:
             #Monotonic
-            return interval(np.cosh(x.start), np.cosh(x.end), is_valid = x.is_valid)
+            start = np.cosh(x.start)
+            end = np.cosh(x.end)
+            return interval(start, end, is_valid=x.is_valid)
     else:
         raise NotImplementedError
+
 
 #Monotonic
 def tanh(x):
@@ -215,38 +225,127 @@ def tanh(x):
     if isinstance(x, (int, float)):
         return interval(np.tanh(x), np.tanh(x))
     elif isinstance(x, interval):
-        return interval(np.tanh(x.start), np.tanh(x.end), is_valid = x.is_valid)
+        return interval(np.tanh(x.start), np.tanh(x.end), is_valid=x.is_valid)
     else:
         raise NotImplementedError
 
-def arcsin(x):
+
+def asin(x):
     """Evaluates the inverse sine of an interval"""
     if isinstance(x, (int, float)):
         if abs(x) > 1:
-            return interval(-np.inf, np.inf, is_valid = False)
+            return interval(-np.inf, np.inf, is_valid=False)
         else:
             return interval(np.arcsin(x), np.arcsin(x))
     elif isinstance(x, interval):
-        if not x.is_valid is False or x.start > 1 or x.end < -1:
-            return interval(-np.inf, np.inf, is_valid = False)
+        if  x.is_valid is False or x.start > 1 or x.end < -1:
+            return interval(-np.inf, np.inf, is_valid=False)
         elif x.start < -1 or x.end > 1:
-            return interval(-np.inf, np.inf, is_valid = None)
+            return interval(-np.inf, np.inf, is_valid=None)
         else:
-            return interval(np.arcsin(x.start), np.arcsin(x.end), is_valid = x.is_valid)
+            start = np.arcsin(x.start)
+            end = np.arcsin(x.end)
+            return interval(start, end, is_valid=x.is_valid)
 
-def arccos(x):
+
+def acos(x):
     """Evaluates the inverse cos of an interval"""
     if isinstance(x, (int, float)):
         if abs(x) > 1:
-            return interval(-np.inf, np.inf, is_valid = False)
+            return interval(-np.inf, np.inf, is_valid=False)
         else:
             return interval(np.arccos(x), np.arccos(x))
     elif isinstance(x, interval):
-        if not x.is_valid is False or x.start > 1 or x.end < -1:
-            return interval(-np.inf, np.inf, is_valid = False)
+        if  x.is_valid is False or x.start > 1 or x.end < -1:
+            return interval(-np.inf, np.inf, is_valid=False)
         elif x.start < -1 or x.end > 1:
-            return interval(-np.inf, np.inf, is_valid = None)
+            return interval(-np.inf, np.inf, is_valid=None)
         else:
-            return interval(np.arccos(x.start), np.arccos(x.end), is_valid = x.is_valid)
+            start = np.arccos(x.start)
+            end = np.arccos(x.end)
+            return interval(start, end, is_valid=x.is_valid)
 
 
+def ceil(x):
+    "Evaluates the ceil of an interval"""
+    if isinstance(x, (int, float)):
+        return interval(np.ceil(x))
+    elif isinstance(x, interval):
+        if x.is_valid is False:
+            return interval(-np.inf, np.inf, is_valid=False)
+        else:
+            start = np.ceil(x.start)
+            end = np.ceil(x.end)
+            if start == end:
+                return interval(start, end, is_valid=x.is_valid)
+            else:
+                return interval(start, end, is_valid=None)
+    else:
+        return NotImplementedError
+
+
+def floor(x):
+    "Evaluates the ceil of an interval"""
+    if isinstance(x, (int, float)):
+        return interval(np.floor(x))
+    elif isinstance(x, interval):
+        if x.is_valid is False:
+            return interval(-np.inf, np.inf, is_valid=False)
+        else:
+            start = np.floor(x.start)
+            end = np.floor(x.end)
+            if start == end:
+                return interval(start, end, is_valid=x.is_valid)
+            else:
+                return interval(start, end, is_valid=None)
+    else:
+        return NotImplementedError
+
+
+def acosh(x):
+    """Evaluates the inverse hyperbolic cosine of an interval"""
+    if isinstance(x, (int, float)):
+        if x < 1:
+            return interval(-np.inf, np.inf, is_valid=False)
+        else:
+            return interval(np.arccosh(x))
+    elif isinstance(x, interval):
+        if x.end < 1:
+            return interval(-np.inf, np.inf, is_valid=False)
+        elif x.start < 1:
+            return interval(-np.inf, np.inf, is_valid=None)
+        else:
+            start = np.arccosh(x.start)
+            end = np.arccosh(x.end)
+            return interval(start, end, is_valid=x.is_valid)
+    else:
+        return NotImplementedError
+
+#Monotonic
+def asinh(x):
+    if isinstance(x, (int, float)):
+        return interval(np.arcsinh(x))
+    elif isinstance(x, interval):
+        start = np.arcsinh(x.start)
+        end = np.arcsinh(x.end)
+        return interval(start, end, is_valid=x.is_valid)
+    else:
+        return NotImplementedError
+
+def atanh(x):
+    if isinstance(x, (int, float)):
+        if abs(x) >= 1:
+            return interval(-np.inf, np.inf, is_valid=False)
+        else:
+            return interval(np.arctanh(x))
+    elif isinstance(x, interval):
+        if  x.is_valid is False or x.start >= 1 or x.end <= -1:
+            return interval(-np.inf, np.inf, is_valid=False)
+        elif x.start <= -1 or x.end >= 1:
+            return interval(-np.inf, np.inf, is_valid=None)
+        else:
+            start = np.arctanh(x.start)
+            end = np.arctanh(x.end)
+            return interval(start, end, is_valid=x.is_valid)
+    else:
+        return NotImplementedError
