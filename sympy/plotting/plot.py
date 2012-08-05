2e3bfc4ecc84d718a3dd0b947c5534f3333f8ad5
Bharath M R <catchmrbharath@gmail.com>
2012-08-05 09:42:56 +0530

    rebasing over master

--- sympy/plotting/plot.py
+++ sympy/plotting/plot.py
@@ -23,33 +23,18 @@ every time you call ``show()`` and the old one is left to the garbage collector.
 """
 
 from inspect import getargspec
-from itertools import repeat, izip
 from sympy import sympify, Expr, Tuple, Dummy
 from sympy.external import import_module
 from sympy.core.compatibility import set_union
-import warnings
 from experimental_lambdify import vectorized_lambdify, lambdify
+from backends import MatplotlibBackend, BaseBackend, TextBackend
 
 #TODO probably all of the imports after this line can be put inside function to
 # speed up the `from sympy import *` command.
 np = import_module('numpy')
 
-# Backend specific imports - matplotlib
-matplotlib = import_module('matplotlib',
-    __import__kwargs={'fromlist':['pyplot', 'cm', 'collections']},
-    min_module_version='1.0.0')
-if matplotlib:
-    plt = matplotlib.pyplot
-    cm = matplotlib.cm
-    LineCollection = matplotlib.collections.LineCollection
-    mpl_toolkits = import_module('mpl_toolkits',
-            __import__kwargs={'fromlist':['mplot3d']})
-    Axes3D = mpl_toolkits.mplot3d.Axes3D
-    art3d = mpl_toolkits.mplot3d.art3d
-
-# Backend specific imports - textplot
-from sympy.plotting.textplot import textplot
-
+#import matplotlib
+matplotlib = import_module('matplotlib')
 
 ##############################################################################
 # The public interface
@@ -992,159 +977,6 @@ class OverloadedSeriesFactory(BaseSeries):
 ##############################################################################
 # Backends
 ##############################################################################
-
-class BaseBackend(object):
-    def __init__(self, parent):
-        super(BaseBackend, self).__init__()
-        self.parent = parent
-
-
-class MatplotlibBackend(BaseBackend):
-    def __init__(self, parent):
-        super(MatplotlibBackend, self).__init__(parent)
-        are_3D = [s.is_3D for s in self.parent._series]
-        if any(are_3D) and not all(are_3D):
-            raise ValueError('The matplotlib backend can not mix 2D and 3D.')
-        elif not any(are_3D):
-            self.fig = plt.figure()
-            self.ax = self.fig.add_subplot(111)
-            self.ax.spines['left'].set_position('zero')
-            self.ax.spines['right'].set_color('none')
-            self.ax.spines['bottom'].set_position('zero')
-            self.ax.spines['top'].set_color('none')
-            self.ax.spines['left'].set_smart_bounds(True)
-            self.ax.spines['bottom'].set_smart_bounds(True)
-            self.ax.xaxis.set_ticks_position('bottom')
-            self.ax.yaxis.set_ticks_position('left')
-        elif all(are_3D):
-            self.fig = plt.figure()
-            self.ax = self.fig.add_subplot(111, projection='3d')
-
-    def process_series(self):
-        parent = self.parent
-
-        for s in self.parent._series:
-            # Create the collections
-            if s.is_2Dline:
-                collection = LineCollection(s.get_segments())
-                self.ax.add_collection(collection)
-            elif s.is_contour:
-                self.ax.contour(*s.get_meshes())
-            elif s.is_3Dline:
-                # TODO too complicated, I blame matplotlib
-                collection = art3d.Line3DCollection(s.get_segments())
-                self.ax.add_collection(collection)
-                x, y, z = s.get_points()
-                self.ax.set_xlim((min(x), max(x)))
-                self.ax.set_ylim((min(y), max(y)))
-                self.ax.set_zlim((min(z), max(z)))
-            elif s.is_3Dsurface:
-                x, y, z = s.get_meshes()
-                collection = self.ax.plot_surface(x, y, z, cmap=cm.jet,
-                                                  rstride=1, cstride=1,
-                                                  linewidth = 0.1)
-            else:
-                raise ValueError('The matplotlib backend supports only '
-                                 'is_2Dline, is_3Dline, is_3Dsurface and '
-                                 'is_contour objects.')
-
-            # Customise the collections with the corresponding per-series
-            # options.
-            if hasattr(s, 'label'):
-                collection.set_label(s.label)
-            if s.is_line and s.line_color:
-                if isinstance(s.line_color, (float, int)) or callable(s.line_color):
-                    color_array = s.get_color_array()
-                    collection.set_array(color_array)
-                else:
-                    collection.set_color(s.line_color)
-            if s.is_3Dsurface and s.surface_color:
-                if matplotlib.__version__ < "1.2.0": #TODO in the distant future remove this check
-                    warnings.warn('The version of matplotlib is too old to use surface coloring.')
-                elif isinstance(s.surface_color, (float,int)) or callable(s.surface_color):
-                    color_array = s.get_color_array()
-                    color_array = color_array.reshape(color_array.size)
-                    collection.set_array(color_array)
-                else:
-                    collection.set_color(s.surface_color)
-
-        # Set global options.
-        # TODO The 3D stuff
-        # XXX The order of those is important.
-        if parent.xscale and not isinstance(self.ax, Axes3D):
-            self.ax.set_xscale(parent.xscale)
-        if parent.yscale and  not isinstance(self.ax, Axes3D):
-            self.ax.set_yscale(parent.yscale)
-        if parent.xlim:
-            self.ax.set_xlim(parent.xlim)
-        if parent.ylim:
-            self.ax.set_ylim(parent.ylim)
-        if not isinstance(self.ax, Axes3D) or matplotlib.__version__ >= '1.2.0': #XXX in the distant future remove this check
-            self.ax.set_autoscale_on(parent.autoscale)
-        if parent.axis_center:
-            val = parent.axis_center
-            if isinstance(self.ax, Axes3D):
-                pass
-            elif val == 'center':
-                self.ax.spines['left'].set_position('center')
-                self.ax.spines['bottom'].set_position('center')
-            elif val == 'auto':
-                xl, xh = self.ax.get_xlim()
-                yl, yh = self.ax.get_ylim()
-                pos_left = ('data', 0) if xl*xh <= 0 else 'center'
-                pos_bottom = ('data', 0) if yl*yh <= 0 else 'center'
-                self.ax.spines['left'].set_position(pos_left)
-                self.ax.spines['bottom'].set_position(pos_bottom)
-            else:
-                self.ax.spines['left'].set_position(('data', val[0]))
-                self.ax.spines['bottom'].set_position(('data', val[1]))
-        if not parent.axis:
-            self.ax.set_axis_off()
-        if parent.legend:
-            self.ax.legend()
-            self.ax.legend_.set_visible(parent.legend)
-        if parent.margin:
-            self.ax.set_xmargin(parent.margin)
-            self.ax.set_ymargin(parent.margin)
-        if parent.title:
-            self.ax.set_title(parent.title)
-        if parent.xlabel:
-            self.ax.set_xlabel(parent.xlabel, position=(1, 0))
-        if parent.ylabel:
-            self.ax.set_ylabel(parent.ylabel, position=(0, 1))
-
-    def show(self):
-        self.process_series()
-        #TODO after fixing https://github.com/ipython/ipython/issues/1255
-        # you can uncomment the next line and remove the pyplot.show() call
-        #self.fig.show()
-        plt.show()
-
-    def save(self, path):
-        self.process_series()
-        self.fig.savefig(path)
-
-    def close(self):
-        plt.close(self.fig)
-
-
-class TextBackend(BaseBackend):
-    def __init__(self, parent):
-        super(TextBackend, self).__init__(parent)
-
-    def show(self):
-        if len(self.parent._series) != 1:
-            raise ValueError('The TextBackend supports only one graph per Plot.')
-        elif not isinstance(self.parent._series[0], LineOver1DRangeSeries):
-            raise ValueError('The TextBackend supports only expressions over a 1D range')
-        else:
-            ser = self.parent._series[0]
-            textplot(ser.expr, ser.start, ser.end)
-
-    def close(self):
-        pass
-
-
 class DefaultBackend(BaseBackend):
     def __new__(cls, parent):
         if matplotlib:
